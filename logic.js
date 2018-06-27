130d1974af3261a5092feccfd0a74945d5b66779
jazmin10 <jazminestrada10@gmail.com>
2018-06-27 17:42:59 -0500

    Added js logic to set new players

--- logic.js
+++ logic.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
   };
 
   var databaseConnectionsRef;
+  var connectedRef;
 
 	var playerOneKey = "";
 	var playerTwoKey = "";
@@ -29,6 +30,63 @@ $(document).ready(function() {
 		$("#results-div").html($("<h2>").text("Waiting..."));
 	}
 
+	// When a player wants to join
+	function addPlayer() {
+		event.preventDefault();
+
+		var newPlayerName = $("#name-input").val().trim();
+		$("#name-input").val("");
+
+		connectedRef.on("value", function(connectionSnap) {
+			var viewer = connectionSnap.val();
+
+			var con = databaseConnectionsRef.push({
+				name: newPlayerName,
+				pick: ""
+			});
+
+			con.onDisconnect().remove();
+		});
+	}
+
+	// Sets the players
+	function setPlayers(playersInfo) {
+		// console.log(playerNumber);
+		console.log(playersInfo);
+
+		var players = Object.keys(playersInfo);
+		console.log(players);
+
+		// If there is only one player...
+		if (players.length === 1) {
+
+			// Store player one key value
+			playerOneKey = players[0];
+
+			// Display player's one name
+			displayPlayer(playersInfo[playerOneKey].name, "#player-one-div");
+
+			$("#results-div").html($("<h2>").text("Waiting..."));
+			$("#player-two-div").html($("<h2>").text("Waiting for player"));
+		}
+		// If there is more than one player...
+		else {
+			// Store player one key value and display name
+			playerOneKey = players[0];
+			displayPlayer(playersInfo[playerOneKey].name, "#player-one-div");
+
+			// Set and display player two and display name
+			playerTwoKey = players[1];
+			displayPlayer(playersInfo[playerTwoKey].name, "#player-two-div");
+		}
+	}
+
+	// Displays player's names in the appropriate section
+	function displayPlayer(name, section) {
+		var p = $("<p>").text("Player's name: " + name);
+		$(section).html(p);
+	}
+
 
 // ========== MAIN PROCESSES ==========
 
@@ -38,6 +96,9 @@ $(document).ready(function() {
 	// Create database reference to connections
 	databaseConnectionsRef = firebase.database().ref("/connections");
 
+	// Store user connection status
+	connectedRef = firebase.database().ref(".info/connected");
+
 	// When database connections change...
 	databaseConnectionsRef.on("value", function(connections) {
 
@@ -45,6 +106,13 @@ $(document).ready(function() {
 		if (connections.numChildren() === 0) {
 			displayNoPlayers();
 		}
+		// If we have at least one player, then set player one
+		else {
+			setPlayers(connections.val());
+		}
 	});
+
+	// When a user wants to join, set the player
+	$("#name-btn").click(addPlayer);
 
 });
