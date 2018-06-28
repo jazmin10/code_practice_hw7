98d2bad76bc013ef745f3dc0c3ae6efa00a9094e
jazmin10 <jazminestrada10@gmail.com>
2018-06-27 22:34:33 -0500

    Set global variables

--- logic.js
+++ logic.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 
+// ================== GLOBAL VARIABLES ==================
+
+// Configuration for Firebase
+	var config = {
+		apiKey: "AIzaSyCa1lcOJMrdYEoXumlkbsOL6WUaFg1LYKU",
+		authDomain: "click-button-4f522.firebaseapp.com",
+		databaseURL: "https://click-button-4f522.firebaseio.com",
+		projectId: "click-button-4f522",
+		storageBucket: "click-button-4f522.appspot.com",
+		messagingSenderId: "794333455197"
+	};
+
+var databaseRef;
+
+// ================== FUNCTIONS ==================
+
+// ================== MAIN PROCESSES ==================
 });
